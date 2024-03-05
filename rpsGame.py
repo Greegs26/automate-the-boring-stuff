cb890b5774f9a749c43324bce81b34bef2ddd834
Greegs26 <greganchristian@gmail.com>
2024-03-05 19:29:01 -0400

    RPS game

--- rpsGame.py
+++ rpsGame.py
@@ -4,4 +4,21 @@ import random, sys
 wins=0
 loss=0
 ties=0
-print(str(wins)+" wins, "+str(loss)+" losses, "+str(ties)+" ties.")
+letters_dict={'r':'ROCK','p':'PAPER','s':'SCISSORS'}
+phrase=", ".join(letters_dict.values())
+print(phrase)
+
+while True:
+    print(str(wins)+" wins, "+str(loss)+" losses, "+str(ties)+" ties.")
+    print("Enter your move: (r)ock (p)aper (s)cissors or (q)uit")
+    response=input()
+    if response.lower()=="q":
+        sys.exit()
+    elif response.lower()=="r" or "p" or "s":
+        print("ok")
+        #versus=random.choice(letters)
+        #if response.lower()=="r":
+            #print("ROCK versus...")
+            #print()
+    else:
+        print("This is not a valid choice.")
