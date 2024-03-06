51aa7f692c47dae27271f30d7e7018c775b5ba5d
Greegs26 <greganchristian@gmail.com>
2024-03-06 19:50:45 -0400

    finished RPS game

--- rpsGame.py
+++ rpsGame.py
@@ -4,23 +4,37 @@ import random, sys
 wins=0
 loss=0
 ties=0
-letters_dict={'r':'ROCK','p':'PAPER','s':'SCISSORS'}
+letters_dict={'r':'ROCK','p':'PAPER','s':'SCISSORS'}    #starting to regret this, not even needed. Will fix this later
 phrase=", ".join(letters_dict.values())
 print(phrase)
 
 while True:
     print(str(wins)+" wins, "+str(loss)+" losses, "+str(ties)+" ties.")
     print("Enter your move: (r)ock (p)aper (s)cissors or (q)uit")
-    response=input()
-    random_key=random.choice(list(letters_dict.keys()))
-    versus=letters_dict[random_key]
-    if response.lower()=="q":
+    user_input=input().lower()
+
+    if user_input=="q":
         sys.exit()
-    elif response.lower()=="r" or "p" or "s":
-        #print("ok")
+
+    elif user_input in letters_dict:
+        user_choice=letters_dict[user_input]
+        print(user_choice+" versus...")
+
+        random_key=random.choice(list(letters_dict.keys()))
+        versus=letters_dict[random_key]
         print(versus)
-        #if response.lower()=="r":
-            #print("ROCK versus...")
-            #print()
+
+        if user_choice==versus:
+            print("It is a tie.")
+            ties=ties+1
+        elif (user_choice=="ROCK" and versus=="SCISSORS") or \
+        (user_choice=="PAPER" and versus=="ROCK") or \
+        (user_choice=="SCISSORS" and versus=="PAPER"):
+            print("You win!")
+            wins=wins+1
+        else:
+            print("You lose...")
+            loss=loss+1
+
     else:
         print("This is not a valid choice.")
