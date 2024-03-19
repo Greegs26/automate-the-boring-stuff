b3bfa07f3de0a99dd7d8d8a0e65e1961857de7ee
Greegs26 <greganchristian@gmail.com>
2024-03-19 20:42:16 -0300

    added new statements to make code better

--- collatzSequence.py
+++ collatzSequence.py
@@ -1,4 +1,7 @@
 import sys, time 
+# 20204/03/19 - trying to add 'try' and 'except' statements to detect if user inputs a non-integer.
+# note: does the collatzSequence work with decimals?? My code certainly doesnt.
+# note2: will my second while loop still work with decimals?? Nope.
 
 def collatz(number):
     if number%2==1: #if number is odd, 3*number+1
@@ -8,10 +11,18 @@ def collatz(number):
     print(number)
     return number
 
-print("Enter number:")
-user_input=int(input())
+while True:
+    print("Enter number:")
+    user_input=input()
 
-while user_input!=1:
-    new_number=collatz(user_input)
+    try:
+        #only work if user_input will be an int
+        number=int(user_input)
+        break #exit the loop
+    except ValueError:
+        print("That is not a valid number.")
+
+while number!=1:
+    new_number=collatz(number)
     time.sleep(0.1) #pause for 0.1 seconds
-    user_input=new_number
+    number=new_number
